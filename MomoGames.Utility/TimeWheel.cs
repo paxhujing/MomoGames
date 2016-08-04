f1fe27bd0d41ec63e3f3181bdff49e2711716c34
benlaishidoudou <benlaishidoudou@163.com>
2016-08-04 10:11:18 +0800

    时间槽元素头部

--- MomoGames.Utility/TimeWheel.cs
+++ MomoGames.Utility/TimeWheel.cs
@@ -1,27 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Linq;
 
 namespace MomoGames.Utility
 {
-    public class TimeWheel
+    public abstract class TimeWheel
     {
         #region Fields
 
         #region init
 
         /// <summary>
-        /// 每走一刻执行的方法。
-        /// </summary>
-        private readonly Action _onTick;
-
-        /// <summary>
         /// 系统计时器。
         /// </summary>
         private readonly Timer _timer;
+
+        /// <summary>
+        /// 一轮总共有多少Tick。
+        /// </summary>
+        private Int32 _ticksPerTurn;
+
+        /// <summary>
+        /// 时间槽列表。
+        /// </summary>
+        private readonly HashSet<TimerSlotElement>[] _slots;
 
         #endregion
 
@@ -32,20 +36,28 @@ namespace MomoGames.Utility
         /// <summary>
         /// 初始化 MomoGames.Utility.TimeWheel 类型实例。
         /// </summary>
-        /// <param name="timePerTick">每一刻度表示的时长，单位毫秒。</param>
-        /// <param name="onTick">每走一刻执行的方法。</param>
-        protected TimeWheel(Double timePerTick, Action onTick)
+        /// <param name="timePerTick">每一刻度表示大于0的时长，单位毫秒。</param>
+        /// <param name="ticksPerTurn">一轮总共的刻度数，必须大于1。</param>
+        protected TimeWheel(Double timePerTick,Int32 ticksPerTurn)
         {
             if (timePerTick <= 0)
             {
                 throw new ArgumentException("timerPerTick");
             }
             this._timePerTick = timePerTick;
-            if(onTick == null)
+
+            if(ticksPerTurn < 1)
             {
-                throw new ArgumentNullException("onTick");
+                throw new ArgumentException("ticksPerTurn");
             }
-            this._onTick = onTick;
+            this._ticksPerTurn = ticksPerTurn;
+
+            this._slots = new HashSet<TimerSlotElement>[ticksPerTurn];
+            for (Int32 i = 0; i < ticksPerTurn; i++)
+            {
+                this._slots[i] = new HashSet<TimerSlotElement>();
+            }
+
             this._timer = new Timer(timePerTick);
             this._timer.Elapsed += _timer_Elapsed;
         }
@@ -70,14 +82,103 @@ namespace MomoGames.Utility
 
         #endregion
 
+        #region CurrentTick
+
+        /// <summary>
+        /// 当前指针指向的Tick。
+        /// </summary>
+        private Int32 _currentTick;
+        /// <summary>
+        /// 获取当前的Tick。
+        /// </summary>
+        public Int32 CurrentTick
+        {
+            get { return this._currentTick; }
+        }
+
+        #endregion
+
         #endregion
 
         #region Methods
 
-        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        #region Register&Unregister
+
+        #endregion
+
+        #region Generate Header
+
+        /// <summary>
+        /// 创建头部。
+        /// </summary>
+        /// <param name="type">头部类型。</param>
+        /// <param name="data">头部数据</param>
+        /// <returns>头部</returns>
+        protected UInt32 NewHeader(TimerSlotElementHeaderType type, UInt16 data)
         {
-            this._onTick();
+            return NewHeader((Byte)type, data);
         }
+
+        /// <summary>
+        /// 创建头部。
+        /// </summary>
+        /// <param name="type">头部类型。</param>
+        /// <param name="data">头部数据</param>
+        /// <returns>头部</returns>
+        protected UInt32 NewHeader(Byte type, UInt16 data)
+        {
+            UInt32 header = 0;
+            header |= (UInt32)(type << 16);
+            header |= data;
+            return header;
+        }
+
+        #endregion
+
+        #region Timer Core
+
+        private void _timer_Elapsed(Object sender, ElapsedEventArgs e)
+        {
+            if ((++this._currentTick) == this._ticksPerTurn)
+            {
+                this._currentTick = 0;
+                ExecuteTurnFinished();
+            }
+            else
+            {
+                ExecuteTickForward(this._currentTick);
+            }
+        }
+
+        /// <summary>
+        /// 刻度每前进移步时执行。
+        /// </summary>
+        /// <param name="tick">当前刻度。</param>
+        private void ExecuteTickForward(Int32 tick)
+        {
+            HashSet<TimerSlotElement> slot = this._slots[tick];
+            lock(slot)
+            {
+                IEnumerable<TimerSlotElement> elements = slot.Where(x => x.Headers.Count == 1);
+                foreach(TimerSlotElement e in elements)
+                {
+                }
+            }
+        }
+
+        private void ExecuteTurnFinished()
+        {
+            //OnTickForward();
+        }
+
+        /// <summary>
+        /// 在指针走完一轮时触发。
+        /// </summary>
+        protected abstract void OnTurnFinished();
+
+        #endregion
+
+        #region Start&Stop
 
         public void Start()
         {
@@ -94,6 +195,8 @@ namespace MomoGames.Utility
                 this._timer.Stop();
             }
         }
+
+        #endregion
 
         #endregion
     }
---
+++ MomoGames.Utility/TimerSlotElement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace MomoGames.Utility
+{
+    /// <summary>
+    /// 计时器槽元素。
+    /// </summary>
+    public struct TimerSlotElement
+    {
+        #region Fields
+
+        private readonly Stack<UInt32> _headers;
+
+        private readonly Action _callback;
+
+        #endregion
+
+        #region Constructors
+
+        public TimerSlotElement(IEnumerable<UInt32> headers,Action callback)
+        {
+            this._headers = new Stack<UInt32>(headers);
+            this._callback = callback;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 头部列表。头列表至少有且只有一个Tick类型的头部。
+        /// --------------------------------------
+        /// |类型8bit|保留8bit|  实际数值16bit   |
+        /// --------------------------------------
+        /// </summary>
+        public UInt32 FirstHeader
+        {
+            get { return this._headers.Peek(); }
+        }
+
+        /// <summary>
+        /// 回调方法。
+        /// </summary>
+        public Action Callback
+        {
+            get { return this._callback; }
+        }
+    }
+}
